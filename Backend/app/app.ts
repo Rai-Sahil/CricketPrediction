76bb0dabe06d5f09ff8901f1520b41fc3467aac9
Sahil Rai <raisahil580@gmail.com>
2023-12-07 01:05:37 -0800

    Typescript-Express server

---
+++ Backend/app/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { Routes } from './server/routes';
+
+class App {
+    public app: express.Application;
+    public routePrv: Routes = new Routes();
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routePrv.routes(this.app)
+    }
+
+    private config(): void {
+        this.app.use(function (req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            next();
+        });
+        // support application/json type post data
+        this.app.use(bodyParser.json());
+        //support application/x-www-form-urlencoded post data
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+}
+
+export default new App().app;
